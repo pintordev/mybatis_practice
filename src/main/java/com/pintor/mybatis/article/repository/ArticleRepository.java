c1859362a7f0709888090604b426124b2586a378
pintor.dev <pintor.dev@gmail.com>
2023-08-22 12:32:30 +0900

    article repository method update

--- src/main/java/com/pintor/mybatis/article/repository/ArticleRepository.java
+++ src/main/java/com/pintor/mybatis/article/repository/ArticleRepository.java
@@ -1,7 +1,9 @@
 package com.pintor.mybatis.article.repository;
 
 import com.pintor.mybatis.article.dto.Article;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
@@ -13,9 +15,29 @@ public interface ArticleRepository {
 
     @Select("""
             <script>
-                SELECT *\s
+                SELECT * 
                 FROM article
             </script>
             """)
     List<Article> getArticles();
+
+    @Insert("""
+            <script>
+                INSERT INTO article 
+                SET createDate = NOW(),
+                modifyDate = NOW(),
+                subject = #{subject},
+                content = #{content}
+            </script>
+            """)
+    void write(@Param("subject") String subject, @Param("content") String content);
+
+    @Select("""
+            <script>
+                SELECT * 
+                FROM article 
+                WHERE id = #{id}
+            </script>
+            """)
+    Article getArticleById(@Param("id") Long id);
 }
